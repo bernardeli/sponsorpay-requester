27eeaee4324bc332b0eb3daf3b791c0eab384cec
Ricardo Bernardeli <ricardobcs@gmail.com>
2012-03-29 03:34:20 -0300

    implemented fetch method that allows user to find data from api

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 # encoding: utf-8
+require 'json'
+
+helpers Helpers
 
 get '/' do
   erb :index
 end
 
-post '/request' do
+post '/fetch' do
+  url = Url.new(params).build
+
+  uri = URI.parse(url)
+  @resp = Net::HTTP.get_response(uri)
+
+  if @resp.code == '200' && Response.valid?(@resp)
+    if @resp.message == "OK"
+      @offers = JSON.parse(@resp.body)['offers']
+      partial :offer, collection: @offers, layout: true
+    else
+      partial :no_offer, layout: true
+    end
+  else
+    erb :error
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,5 +9,67 @@ describe "SponsorPay Requester" do
       last_response.status.should == 200
     end
   end
+
+  describe "POST fetch" do
+    let(:url) { 'http://sponsorpay.com/api' }
+
+    before(:each) do
+      Url.any_instance.stub(:build).and_return(url)
+    end
+
+    context "when response is other than 200" do
+      before(:each) do
+        FakeWeb.register_uri :get, url, code: [400, 'Bad Request']
+      end
+
+      it "renders error's page" do
+        post :fetch, uid: 'player', pub0: 'campaign2', page: 1
+        last_response.body.should include "Something went wrong"
+      end
+    end
+
+    context "when response code is 200" do
+      before(:each) do
+        FakeWeb.register_uri :get, url, status: [200, 'OK'], body: File.open('./spec/support/success.json').read
+      end
+
+      context "when message is No Content" do
+        before(:each) do
+          FakeWeb.register_uri :get, url, status: [200, 'No Content']
+          Response.stub(:valid?).and_return(true)
+        end
+
+        it "renders no offer partial" do
+          post :fetch, uid: 'player', pub0: 'campaign2', page: 1
+          last_response.body.should include "<div class='no_offers'>"
+          last_response.body.should include "No offers"
+        end
+
+      end
+
+      context "when response is not valid" do
+        before(:each) { Response.stub(:valid?).and_return(false) }
+
+        it "renders error" do
+          post :fetch, uid: 'player', pub0: 'campaign2', page: 1
+          last_response.status.should == 200
+          last_response.body.should include "Something went wrong"
+        end
+      end
+
+      context "when response is valid" do
+        before(:each) { Response.stub(:valid?).and_return(true) }
+
+        it "renders offers" do
+          post :fetch, uid: 'player', pub0: 'campaign2', page: 1
+          last_response.status.should == 200
+          last_response.body.should include "<div class='offer'>"
+          last_response.body.should include "<div class='title'>Solitaire - Das klassische Spiel</div>"
+          last_response.body.should include "<div class='payout'>20240</div>"
+          last_response.body.should include "<div class='thumbnail'>http://acp-production.s3.amazonaws.com/app_icons/248/big_icon.png</div>"
+        end
+      end
+    end
+  end
 end
 
